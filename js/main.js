b93b1073441ee74a280a08b73222d050ea907f8e
Justyna Bodurka <bodurkajustyna@gmail.com>
2021-11-21 23:39:14 +0100

    feat: Basket functionality

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
+const BASKET = []
+let PRODUCTS_LIST
+
 fetch("https://raw.githubusercontent.com/alexsimkovich/patronage/main/api/data.json")
   .then((resp) => resp.json())
   .then((resp) => initialize(resp))
-  .catch((error) => {
-    console.log("nie udało się pobrać");
-  });
+  .catch((error) => console.log("nie udało się pobrać"));
 
-function initialize(array) {
-  console.log(array);
-  array.forEach((element) => {
+function initialize (array) {
+  PRODUCTS_LIST = array
+
+  renderProducts()
+  addProductListeners()
+}
+
+function addProductListeners () {
+  const buttons = document.querySelectorAll('.product .button')
+  buttons.forEach(button => button.addEventListener('click', addToBasket))
+}
+
+function renderProducts () {
+  PRODUCTS_LIST.forEach((element) => {
     const list = document.querySelector(".products-list");
+    
     list.insertAdjacentHTML("beforeend",`
       <div class="product">
         <div  class="product__image" style="background-image: url('${element.image}');"></div>
@@ -17,8 +30,45 @@ function initialize(array) {
           <div class="product__price">${element.price}</div>
           <div class="product__ingredients">${element.ingredients.join(", ")}</div>
 
-          <button class="button">zamów</button>
+          <button class="button" data-product-id="${element.id}">zamów</button>
       </div>
     </div> `);
-  });
+  }) 
+}
+
+
+function addToBasket (e) {
+  const productId = parseInt(e.target.getAttribute('data-product-id'))
+  const product = PRODUCTS_LIST.find(el => el.id === productId)
+
+  if(!product) return alert('siem nie udao')
+
+  const productInBasket = BASKET.find(el => el.id === productId)
+  
+  if (productInBasket) {
+    productInBasket.quantity++
+  } else {
+    BASKET.push({
+      id: product.id,
+      price: product.price,
+      title: product.title,
+      quantity: 1
+    })
+  }
+
+  renderBasket()
+}
+
+function renderBasket () {
+  const basketList = document.querySelector('.basket__list')
+  basketList.innerHTML = ''
+  console.log('renderBasket', BASKET)
+  BASKET.forEach(product => {
+    basketList.insertAdjacentHTML("beforeend",`<div class="basket__item">
+    <div class="basket__item-name">${product.title}</div>
+    <div class="basket__item-price">${product.price}</div>
+    <div class="basket__item-quantity">${product.quantity}</div>
+    <div class="basket__item-options"><button>usun</button></div>
+  </div>`)
+  })
 }
